f64468efd14c9a74f64a9d0f7aafd574131068a3
wissem <149834224+wissamn19@users.noreply.github.com>
2025-08-20 20:57:47 +0200

    Create length of the diagonal of the square.py

---
+++ length of the diagonal of the square.py
@@ -0,0 +1,7 @@
+a = float(input('Enter the A side of the sqaure:'))
+
+b = float(input('Entre the B side of the square:'))
+
+c = math.sqrt(pow(a , 2) + pow(b , 2))
+
+print(f'The length of the diagonal of the square is :{round(c , 2)}cm ')
